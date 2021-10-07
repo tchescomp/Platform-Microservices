cfffb45420e63271c7b1cb6dd893952f6f6341d7
tchescomp <31317605+tchescomp@users.noreply.github.com>
2021-10-07 09:43:59 -0300

    First commit

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        #region Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+
+        void CreatePlatform(Platform plat);
+
+        bool PlatformExits(int platformId);
+
+        bool ExternalPlatformExists(int externalPlatformId);
+        #endregion
+
+        #region Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        
+        Command GetCommand(int platformId, int commandId);
+
+        void CreateCommand(int platformId, Command command);
+        #endregion
+
+
+    }
+}
